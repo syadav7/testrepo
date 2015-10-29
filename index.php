bb12769f8c142ad1b9eb1efe679cd00f55c799a6
sumitmalik1983 <smalik@techcraftz.com>
2015-10-29 12:38:20 +0530

    added a space

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 ﻿<!DOCTYPE html>
 <html>
-<head>
+<head> 
 <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
 <meta http-equiv="X-UA-Compatible" content="IE=edge">
